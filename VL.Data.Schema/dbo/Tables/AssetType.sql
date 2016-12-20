f753df457ea006862cdc1a008d886ee6c2b4242e
J. Henry Rodriguez <josephhrodriguez@hotmail.com>
2016-12-20 14:41:19 -0800

    Updated schema

--- VL.Data.Schema/dbo/Tables/AssetType.sql
+++ VL.Data.Schema/dbo/Tables/AssetType.sql
@@ -2,12 +2,14 @@
     [ID]               INT          IDENTITY (1, 1) NOT NULL,
     [Code]             VARCHAR (50) NOT NULL,
     [Name]             VARCHAR (50) NOT NULL,
-    [CreatedByUserID]  VARCHAR (50) NOT NULL,
-    [CreatedDate]      DATETIME     NOT NULL,
-    [ModifiedByUserID] VARCHAR (50) NOT NULL,
-    [ModifiedDate]     DATETIME     NOT NULL,
+    [CreatedByUserID]  VARCHAR (50) CONSTRAINT [DF_AssetType_CreatedByUserID] DEFAULT ('_admin') NOT NULL,
+    [CreatedDate]      DATETIME     CONSTRAINT [DF_AssetType_CreatedDate] DEFAULT (getdate()) NOT NULL,
+    [ModifiedByUserID] VARCHAR (50) CONSTRAINT [DF_AssetType_ModifiedByUserID] DEFAULT ('_admin') NOT NULL,
+    [ModifiedDate]     DATETIME     CONSTRAINT [DF_AssetType_ModifiedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_AssetType] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
--- VL.Data.Schema/dbo/Tables/BadgeType.sql
+++ VL.Data.Schema/dbo/Tables/BadgeType.sql
@@ -2,12 +2,14 @@
     [ID]               INT          IDENTITY (1, 1) NOT NULL,
     [Code]             VARCHAR (10) NOT NULL,
     [Name]             VARCHAR (50) NOT NULL,
-    [CreatedByUserID]  VARCHAR (50) NOT NULL,
-    [CreatedDate]      DATETIME     NOT NULL,
-    [ModifiedByUserID] VARCHAR (50) NOT NULL,
-    [ModifedDate]      DATETIME     NOT NULL,
+    [CreatedByUserID]  VARCHAR (50) CONSTRAINT [DF_BadgeType_CreatedByUserID] DEFAULT ('_admin') NOT NULL,
+    [CreatedDate]      DATETIME     CONSTRAINT [DF_BadgeType_CreatedDate] DEFAULT (getdate()) NOT NULL,
+    [ModifiedByUserID] VARCHAR (50) CONSTRAINT [DF_BadgeType_ModifiedByUserID] DEFAULT ('_admin') NOT NULL,
+    [ModifedDate]      DATETIME     CONSTRAINT [DF_BadgeType_ModifedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_BadgeType] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
 
 
